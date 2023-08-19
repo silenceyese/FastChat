4c16ccd7aa2bfd8816eeab73089899555bdd3b9f
tommy <46382167@qq.com>
2023-08-19 18:04:50 +0800

    1

--- tests/launch_openai_api_test_server.py
+++ tests/launch_openai_api_test_server.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     for i, model_path in enumerate(models):
         launch_process(
             f"CUDA_VISIBLE_DEVICES={i} python3 -m fastchat.serve.model_worker "
-            f"--model-path {model_path} --port {30000+i} --model-names 'gpt-3.5-turbo' --limit-worker-concurrency 5 --worker http://localhost:{30000+i}"
+            f"--model-path {model_path} load_8bit True --port {30000+i} --model-names 'gpt-3.5-turbo' --limit-worker-concurrency 5 --worker http://localhost:{30000+i}"
         )
 
     while True:
