ecc49c5a7a95b1460ee8da8f3c50ad138cd5cabb
tommy <46382167@qq.com>
2023-08-19 14:52:03 +0800

    调整开服命令

--- tests/launch_openai_api_test_server.py
+++ tests/launch_openai_api_test_server.py
@@ -10,19 +10,21 @@ def launch_process(cmd):
 
 if __name__ == "__main__":
     launch_process("python3 -m fastchat.serve.controller")
-    launch_process("python3 -m fastchat.serve.openai_api_server")
+    launch_process("python3 -m fastchat.serve.openai_api_server --host 0.0.0.0")
 
     models = [
-        "lmsys/vicuna-7b-v1.3",
-        "lmsys/fastchat-t5-3b-v1.0",
-        "THUDM/chatglm-6b",
-        "mosaicml/mpt-7b-chat",
+        # "lmsys/vicuna-7b-v1.3",
+        # "lmsys/fastchat-t5-3b-v1.0",
+        # "../../chatglm2-6b",
+        # "mosaicml/mpt-7b-chat",
+        # "../../Qwen-7B-Chat",
+        "../../Qwen-7B-Chat",
     ]
 
     for i, model_path in enumerate(models):
         launch_process(
             f"CUDA_VISIBLE_DEVICES={i} python3 -m fastchat.serve.model_worker "
-            f"--model-path {model_path} --port {30000+i} --worker http://localhost:{30000+i}"
+            f"--model-path {model_path} --port {30000+i} --model-names 'gpt-3.5-turbo' --limit-worker-concurrency 5 --worker http://localhost:{30000+i}"
         )
 
     while True:
